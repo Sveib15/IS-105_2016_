e21e23fdce508861455949a54e2673af8796a5de
Sveib15 <sveinbruce132@gmail.com>
2016-02-05 19:57:06 +0100

    fixed the code in the dekoder-files and changed the name of dekoderspekalusjon. also changed something in the word document

--- oppgaver05.02/dekoder1.py
+++ oppgaver05.02/dekoder1.py
@@ -1,31 +1,32 @@
 Code = input("hva er fakultetskoden?")
 
 indeks=0
-n=5
+n=3
 inpCode=[Code[i:i+n]for i in range(0, len(Code), n)]
 
 
 while (indeks<len(inpCode)):
-    if (inpCode[indeks]=="00000"):
+    if (inpCode[indeks]=="000"):
         print "Okonomi og samfunnsvitenskap"
         indeks=indeks+1
-    elif (inpCode[indeks]=="10000"):
+    elif (inpCode[indeks]=="100"):
         print "Teknologi og realfag"
         indeks=indeks+1
-    elif (inpCode[indeks]=="11000"):
+    elif (inpCode[indeks]=="110"):
         print "Helse- og idrettsfag"
         indeks=indeks+1
-    elif (inpCode[indeks]=="11100"):
+    elif (inpCode[indeks]=="111"):
         print "Humaniora og pedagogikk"
         indeks=indeks+1
-    elif (inpCode[indeks]=="11110"):
+    elif (inpCode[indeks]=="011"):
         print "Laererutdanningen"
         indeks=indeks+1
-    elif (inpCode[indeks]=="11111"):
+    elif (inpCode[indeks]=="001"):
         print "Kunstfag"
         indeks=indeks+1
     else :
         print "vennligst skriv en passede kode"
         indeks=indeks+1
+
 
 
